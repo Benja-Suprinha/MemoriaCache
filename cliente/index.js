a488fd430576e500c9b09efbfee5753590ae33b7
Benja Supra <benjamin.ojeda@mail.udp.cl>
2022-04-21 21:32:43 -0400

    conexion api-redis

--- cliente/index.js
+++ cliente/index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const redis = require('redis');
 
 app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Api listening at http://localhost:${port}`)
 });
 
+(async () => {
+  const client = redis.createClient();
+
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+
+  await client.set('key', 'value');
+  const value = await client.get('key');
+})();
+
 app.get('/', (req,res) => {
-	res.send("Hello word");	
+	res.send("Hello word");
 });
