8222fbf3d86bea329c57a680428e6ae0c6bcc920
Benja Supra <benjamin.ojeda@mail.udp.cl>
2022-05-01 22:38:58 -0400

    creacion server grpc

--- cliente/index.js
+++ cliente/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const session = require('express-session')
-const app = express()
-const port = 8000
+const connectRedis = require('connect-redis');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 const redis = require('redis');
 
+const app = express()
+const port = 8000
+
 app.use(express.static('public'));
-app.listen(port, () => {
-    console.log(`Api listening at http://localhost:${port}`)
-});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const RedisStore = connectRedis(session);
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,   
+    host: process.env.REDIS_HOST, 
     port: 6379
 });
 
@@ -24,6 +25,21 @@ redisClient.on("error", function(error) {
   console.error(error);
 });
 
-app.get('/', (req,res) => {
-	res.send("Hello word");
+app.use(session({
+  store: new RedisStore({ client: redisClient }),
+  secret: 'mysecret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+      maxAge: 1000
+  }
+}));
+
+app.get("/", (req, res) => {
+  res.send("hello world")
+});
+
+
+app.listen(port, () => {
+  console.log(`Api listening at http://localhost:${port}`)
 });
